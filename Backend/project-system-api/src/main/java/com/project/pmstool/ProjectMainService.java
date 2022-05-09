7ffcd2dbb48896f4a6f8c947b54c55080e59e8fe
Rintu Mazumder <Rintu.Mazumder@neudesic.com>
2022-05-09 13:14:25 +0530

    updated

--- Backend/project-system-api/src/main/java/com/project/pmstool/ProjectMainService.java
+++ Backend/project-system-api/src/main/java/com/project/pmstool/ProjectMainService.java
@@ -2,8 +2,6 @@ package com.project.pmstool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,7 +13,6 @@ public class ProjectMainService {
 	}
 
 	@Bean
-	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
